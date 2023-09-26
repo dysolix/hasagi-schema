324f228f3d3b4bb4a5f7c5296bc2e3384060df57
Felix Matenar <contact@dysolix.dev>
2023-09-26 23:52:33 +0200

    Initial commit

---
+++ src/generate-openapi-v3.ts
@@ -0,0 +1,248 @@
+import axios from "axios";
+import HasagiLiteClient from "@hasagi/core";
+import { Type, Endpoint, Event, ExtendedHelp } from "./types.js";
+import { OpenAPIObject, OperationObject, ParameterObject, ReferenceObject, RequestBodyObject, ResponseObject, SchemaObject, SecurityRequirementObject } from "./open-api-types.js";
+
+function getRootType(input: Type): SchemaObject | ReferenceObject {
+    const isObject = input.fields.length > 0;
+    const isEnum = input.values.length > 0;
+
+    const required: string[] = [];
+    const properties: Record<string, SchemaObject | ReferenceObject> = {}
+    input.fields.forEach(f => {
+        properties[f.name] = getType(f)!;
+        if(!f.optional)
+            required.push(f.name)
+    })
+
+    return {
+        type: isEnum ? "string" : "object",
+        description: input.description,
+        properties: isObject ? properties : undefined,
+        enum: isEnum ? input.values.sort((v1, v2) => v2.value - v1.value).map(v => v.name) : undefined,
+        additionalProperties: !isObject && !isEnum,
+        required
+    }
+
+    /* if (isRoot) {
+        
+    } else {
+        let type: string | undefined = input.type.type;
+
+        switch (type) {
+            case "string":
+                return { type: "string" }
+
+            case "uint8":
+            case "uint16":
+            case "uint32":
+            case "uint64":
+                return { type: "integer", format: type, minimum: 0 }
+            case "int8":
+            case "int16":
+            case "int32":
+            case "int64":
+                return { type: "integer", format: type }
+            case "bool":
+                return { type: "boolean" }
+            case "double":
+            case "float":
+                return { type: "number", format: type }
+
+            case "vector":
+                const arrElementType = getType(input.type.elementType);
+                return {
+                    type: "array",
+                    items: arrElementType
+                }
+            case "map":
+                const mapElementType = getType(input.type.elementType);
+                return {
+                    additionalProperties: mapElementType
+                }
+
+            default:
+                return { $ref: `#/components/schemas/${type}` }
+        }
+    } */
+}
+
+function getType(input: { type: { elementType: string; type: string; }; } | string): SchemaObject | ReferenceObject | undefined {
+    const type = typeof input === "string" ? input : input.type.type;
+
+    switch (type) {
+        case "string":
+            return { type: "string" }
+        case "uint8":
+        case "uint16":
+        case "uint32":
+        case "uint64":
+            return { type: "integer", format: type, minimum: 0 }
+        case "int8":
+        case "int16":
+        case "int32":
+        case "int64":
+            return { type: "integer", format: type }
+        case "bool":
+            return { type: "boolean" }
+        case "double":
+        case "float":
+            return { type: "number", format: type }
+        case "vector":
+            return { type: "array", items: getType((input as Type["fields"][number]).type.elementType) }
+        case "map": {
+            return { type: "object", additionalProperties: getType((input as Type["fields"][number]).type.elementType) }
+        }
+        case "object":
+            return { type: "object", additionalProperties: true }
+        case "":
+            return undefined;
+        default:
+            return { $ref: `#/components/schemas/${type}` }
+    }
+}
+
+export async function generateOpenAPIv3(schema: ExtendedHelp) {
+    const client = new HasagiLiteClient();
+    await client.connect();
+    const region = await client.request({ method: "get", url: "/riotclient/region-locale" });
+    const version: string = await axios.get(`https://ddragon.leagueoflegends.com/realms/${region.webRegion}.json`).then(res => res.data.v);
+
+    const openAPISchema: OpenAPIObject = {
+        openapi: "3.0.0",
+        info: {
+            title: "LCU SCHEMA",
+            description: "Auto-generated using LCU's /help endpoint. Some endpoints are not included because their /help response is missing necessary fields.",
+            version
+        },
+        components: { schemas: {} },
+        paths: {}
+    }
+
+    let tags: string[] = [];
+    schema.types.forEach(type => openAPISchema.components!.schemas![type.name] = getRootType(type));
+    schema.functions.forEach(func => {
+        if (func.method === null || func.path === null) {
+            console.log(`Function '${func.name}' does not have a http method or path.`)
+            return;
+        }
+
+        openAPISchema.paths[func.path] = openAPISchema.paths[func.path] ?? {}
+        const operation = endpointToOperation(func, openAPISchema);
+        tags.push(...operation.tags!.filter(tag => !tags.includes(tag)))
+        // @ts-expect-error
+        openAPISchema.paths[func.path][func.method.toLowerCase()] = operation;
+    })
+
+    openAPISchema.tags = tags.map(tag => {
+        return {
+            name: tag
+        }
+    }).sort((t1, t2) => {
+        if (t1.name.includes("Plugin") && !t2.name.includes("Plugin"))
+            return 1;
+
+        if (!t1.name.includes("Plugin") && t2.name.includes("Plugin"))
+            return -1;
+
+        return t1.name.localeCompare(t2.name)
+    })
+
+    return openAPISchema;
+}
+
+function endpointToOperation(endpoint: Endpoint, schema: OpenAPIObject): OperationObject {
+    let parameters: (ParameterObject | ReferenceObject)[] = undefined!;
+    let response: ResponseObject = { description: "Success response" };
+    let requestBody: RequestBodyObject = undefined!;
+
+    if (endpoint.method === "GET") {
+        parameters = endpoint.pathParams?.map(param => {
+            const p = endpoint.arguments.find(arg => arg.name.replace("+", "") === param.replace("+", ""));
+
+            return {
+                in: "path",
+                name: param,
+                required: true,
+                schema: p === undefined ? { type: "string" } : getType(p)
+            }
+        }) ?? [];
+
+        const params: ParameterObject[] = endpoint.arguments.slice(parameters.length).flatMap(arg => {
+            const type = getType(arg);
+            if ("$ref" in type!) {
+                const schemaObject = schema!.components!.schemas![type.$ref.split("/").at(-1)!] as SchemaObject;
+                if (schemaObject.properties)
+                    return Object.entries(schemaObject.properties).map(entry => {
+                        return {
+                            in: "query",
+                            name: entry[0],
+                            type: entry[1],
+                        } as ParameterObject
+                    })
+
+                return {
+                    in: "query",
+                    schema: type,
+                    name: arg.name
+                } as ParameterObject
+            } else {
+                return {
+                    in: "query",
+                    required: !arg.optional,
+                    name: arg.name,
+                    schema: getType(arg)
+                }
+            }
+        })
+
+        parameters.push(...params);
+
+        const returnType = getType({ type: endpoint.returns })
+        response = { content: { "application/json": { schema: returnType } }, description: "Success response" }
+    } else {
+        parameters = endpoint.pathParams?.map(param => {
+            const p = endpoint.arguments.find(arg => arg.name.replace("+", "") === param.replace("+", ""));
+
+            return {
+                in: "path",
+                name: param,
+                required: true,
+                schema: p === undefined ? { type: "string" } : getType(p)
+            }
+        }) ?? [];
+
+        const bodyType = endpoint.arguments.slice(parameters.length).at(0);
+        if (bodyType)
+            requestBody = {
+                content: {
+                    "application/json": {
+                        schema: getType(bodyType)
+                    }
+                }
+            }
+
+        const returnType = getType({ type: endpoint.returns })
+        response = returnType !== undefined ? { content: { "application/json": { schema: returnType } }, description: "Success response" } : { description: "Success response" }
+    }
+
+    const tags: string[] = [];
+    if(endpoint.path?.startsWith("/lol-"))
+        tags.push("Plugin " + endpoint.path.split("/")[1])
+    else if(endpoint.path?.startsWith("/{plugin}"))
+        tags.push("asset-serving");
+    else 
+        tags.push(endpoint.path!.split("/")[1])
+
+    const _tags = endpoint.tags;
+    return {
+        operationId: endpoint.name,
+        description: endpoint.description,
+        tags: _tags,
+        parameters,
+        requestBody,
+        responses: {
+            "2XX": response
+        },
+    } as OperationObject
+}
---
+++ src/openapi-schema.ts
@@ -0,0 +1,159 @@
+import { ComponentsObject, ExternalDocumentationObject, InfoObject, OpenAPIObject, OperationObject, ParameterObject, PathsObject, ReferenceObject, RequestBodyObject, ResponseObject, SchemaObject, SecurityRequirementObject, ServerObject, TagObject } from "./open-api-types.js";
+import { DataStructure } from "./types.js";
+import { formatFieldName } from "./util.js";
+
+const TYPE_OVERRIDES: { [key: string]: { rename?: string, definition?: string } } = {
+"PluginResourceEvent": { definition:
+`interface PluginResourceEvent<DataType = unknown> {
+	uri: string
+	eventType: {{namespace}}PluginResourceEventType
+	data: DataType
+}` }
+}
+
+export default class OpenAPISchema implements OpenAPIObject {
+    openapi!: string;
+    info!: InfoObject;
+    servers?: ServerObject[] | undefined;
+    paths!: PathsObject;
+    components?: ComponentsObject | undefined;
+    security?: SecurityRequirementObject[] | undefined;
+    tags?: TagObject[] | undefined;
+    externalDocs?: ExternalDocumentationObject | undefined;
+
+    constructor(schema: OpenAPIObject) {
+        Object.assign(this, schema);
+    }
+
+    getTypeScriptInterfaces(namespace?: string) {
+        if (this.components?.schemas === undefined)
+            return [];
+
+
+        return Object.entries(this.components.schemas).map(([name, schema]) => {
+            if(TYPE_OVERRIDES[name] && TYPE_OVERRIDES[name].definition)
+                return `export ${TYPE_OVERRIDES[TYPE_OVERRIDES[name].rename ?? name].definition}`.replaceAll("{{namespace}}", namespace !== undefined ? `${namespace}.` : "")
+
+            if ("$ref" in schema) {
+                return `export type ${name} = ${schema.$ref.split("/").at(-1)}`
+            } else if (schema.enum !== undefined) {
+                return `export type ${name} = ${schema.enum.map(e => `"${e}"`).join(" | ")}`
+            } else if (schema.type === "object") {
+                const _interface = [`export interface ${name.replaceAll("-", "_")} {`]
+                if (schema.additionalProperties)
+                    _interface.push(`\t[key: string | number]: ${schema.additionalProperties === true ? "any" : getTypeBySchemaObject(schema.additionalProperties, namespace)}`)
+                if (schema.properties)
+                    Object.entries(schema.properties).forEach(([name, property]) => {
+                        const required = schema.required?.includes(name) ?? false;
+                        _interface.push(`\t${formatFieldName(name)}${required ? "" : "?"}: ${getTypeBySchemaObject(property, namespace)}`)
+                    })
+
+                _interface.push("}");
+                return _interface.join("\n");
+            }
+
+            return getTypeBySchemaObject(schema, namespace)
+        });
+    }
+
+    getEndpointInterfaces(namespace?: string) {
+        const endpoints: Record<string, Record<string, ReturnType<typeof getMethodInfo>>> = {  }
+        Object.entries(this.paths).forEach(([path, info]) => {
+            endpoints[path] = {
+                get: getMethodInfo(info.get, namespace),
+                post: getMethodInfo(info.post, namespace),
+                put: getMethodInfo(info.put, namespace),
+                patch: getMethodInfo(info.patch, namespace),
+                head: getMethodInfo(info.head, namespace),
+                delete: getMethodInfo(info.delete, namespace),
+            }
+        })
+
+        let endpointsStr = "export interface LCUEndpoints {\n";
+        Object.entries(endpoints).map(([path, r]) => {
+            endpointsStr += `\t"${path}": {\n`
+            Object.entries(r).map(([method, types]) => endpointsStr += types !== undefined ? `\t\t${method}: { Parameters: ${types!.Parameter}, Body: ${types!.Body}, Response: ${types!.Response} }\n` : "")
+            endpointsStr += "\t},\n"
+        }).join("\t");
+        endpointsStr += "}"
+
+        return endpointsStr + "\n\n" + 
+`// @ts-expect-error
+export type LCUEndpoint<Method extends HttpMethod, Path extends EndpointsWithMethod<Method>> = LCUEndpointBodyType<Method, Path> extends never ? (...args: [...LCUEndpoints[Path][Method]["Parameters"]]) => Promise<LCUEndpointResponseType<Method, Path>> : (...args: [...LCUEndpoints[Path][Method]["Parameters"], body: LCUEndpointBodyType<Method, Path>]) => Promise<LCUEndpointResponseType<Method, Path>>
+// @ts-expect-error
+export type LCUEndpointResponseType<Method extends HttpMethod, Path extends EndpointsWithMethod<Method>> = LCUEndpoints[Path][Method]["Response"]
+// @ts-expect-error
+export type LCUEndpointBodyType<Method extends HttpMethod, Path extends EndpointsWithMethod<Method>> = LCUEndpoints[Path][Method]["Body"]
+
+export type EndpointsWithMethod<Method extends HttpMethod> = { [K in keyof LCUEndpoints]: LCUEndpoints[K] extends { [key in Method]: { } } ? K : never }[keyof LCUEndpoints]
+
+export type HttpMethod = "delete" | "get" | "head" | "patch" | "post" | "put";`
+    }
+}
+
+function getMethodInfo(info: OperationObject | undefined, namespace?: string) {
+    if(!info)
+        return undefined;
+
+    const pathParams = info.parameters?.filter(p => (p as ParameterObject).in === "path") as ParameterObject[] ?? [];
+    const pathParamsObject = `${pathParams.map(p => `${p.name.replaceAll("-", "_").replaceAll("+", "")}: ${getTypeBySchemaObject(p.schema, namespace)}`).join(", ")}`;
+
+    const queryParams = info.parameters?.filter(p => (p as ParameterObject).in === "query") as ParameterObject[] ?? [];
+    const queryParamsObject = `{ ${queryParams.map(p => `"${p.name}"${p.required ? "" : "?"}: ${getTypeBySchemaObject(p.schema, namespace)}`).join(", ")} }`;
+
+    return {
+        Parameter: `[${pathParamsObject}${queryParams.length > 0 ? `${pathParams.length > 0 ? ", " : ""}params${queryParams.some(p => p.required) ? "" : "?"}: ${queryParamsObject}` : ""}]`,
+        Body: info.requestBody !== undefined && "content" in info.requestBody ? getTypeBySchemaObject((info.requestBody as RequestBodyObject).content["application/json"]["schema"]!, namespace) : "never",
+        Response: info.responses["2XX"] !== undefined && "content" in info.responses["2XX"] ? getTypeBySchemaObject((info.responses["2XX"] as ResponseObject)["content"]!["application/json"]["schema"]!, namespace) : "void"
+    }
+}
+
+function getTypeBySchemaObject(schema: SchemaObject | ReferenceObject | undefined, namespace?: string): string {
+    if(schema === undefined)
+        return "void";
+
+    if ("$ref" in schema)
+        return `${namespace !== undefined ? `${namespace}.` : ""}${schema.$ref.split("/").at(-1)!}`;
+
+    switch (schema.type) {
+        case "string":
+            return "string";
+        case "number":
+        case "integer":
+            return "number";
+        case "boolean":
+            return "boolean";
+        case "array":
+            return getArrayTypeBySchemaObject(schema, namespace).split("\n").join("\n\t");
+        case "object":
+            return getObjectTypeBySchemaObject(schema, namespace).split("\n").join("\n\t");
+        default:
+            return `${namespace}.${TYPE_OVERRIDES[schema.type!].rename ? TYPE_OVERRIDES[schema.type!].rename : schema.type}` ?? "unknown";
+    }
+}
+
+function getArrayTypeBySchemaObject(schema: SchemaObject, namespace?: string): string {
+    if (schema.type !== "array")
+        throw new Error();
+
+    if (schema.items)
+        return `${getTypeBySchemaObject(schema.items, namespace)}[]`;
+
+    return "any[]";
+}
+
+function getObjectTypeBySchemaObject(schema: SchemaObject, namespace?: string): string {
+    const _interface = [`{`]
+    if(schema.additionalProperties && !schema.properties)
+        return schema.additionalProperties === true ? "unknown" : `Record<string, ${getTypeBySchemaObject(schema.additionalProperties, namespace)}>`
+
+    if (schema.additionalProperties)
+        _interface.push(`\t[key: string | number]: ${schema.additionalProperties === true ? "any" : getTypeBySchemaObject(schema.additionalProperties, namespace)}`)
+    if (schema.properties)
+        Object.entries(schema.properties).forEach(([name, property]) => {
+            const required = schema.required?.includes(name) ?? false;
+            _interface.push(`\t${formatFieldName(name)}${required ? "" : "?"}: ${getTypeBySchemaObject(property, namespace).split("\n").join("\n\t")}`)
+        })
+    _interface.push("}");
+    return _interface.join("\n");
+}
